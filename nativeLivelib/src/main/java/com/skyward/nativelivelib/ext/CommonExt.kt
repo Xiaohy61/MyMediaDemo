39d249cb559b0f7a2b338d387c662fa119bd79b9
xiaohuiye <316639182@qq.com>
2023-02-14 17:07:51 +0800

    修复前摄推流问题

--- nativeLivelib/src/main/java/com/skyward/nativelivelib/ext/CommonExt.kt
+++ nativeLivelib/src/main/java/com/skyward/nativelivelib/ext/CommonExt.kt
@@ -14,20 +14,23 @@ import com.skyward.nativelivelib.camera2.ICamera2
 
 fun Context.getCameraRotation(cameraType: ICamera2.CameraType):Int{
     val rotation = when ((getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.rotation) {
+       //竖屏
         Surface.ROTATION_0 ,Surface.ROTATION_180-> {
             if(cameraType == ICamera2.CameraType.BACK){
                 90
             }else{
+                //前摄
                 270
             }
         }
+        //横屏
         Surface.ROTATION_90 ,Surface.ROTATION_270 ->{
-            if(cameraType == ICamera2.CameraType.BACK){
-                0
-            }else{
-                180
-            }
-
+//            if(cameraType == ICamera2.CameraType.BACK){
+//                0
+//            }else{
+//                0
+//            }
+            0
         }
         else -> {
             90
